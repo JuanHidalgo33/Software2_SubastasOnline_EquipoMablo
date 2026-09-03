bb88db2cc3cad1a57e72d9e9938be0048e550d66
JuanHidalgo33 <andrehidalgo933@gmail.com>
2026-09-02 23:24:14 -0500

    Added errors and ports

---
+++ src/application/errors/ApplicationError.ts
@@ -0,0 +1,10 @@
+export class ApplicationError extends Error {
+  constructor(
+    public readonly code: string,
+    message: string,
+    public readonly status: number,
+  ) {
+    super(message);
+    this.name = 'ApplicationError';
+  }
+}
